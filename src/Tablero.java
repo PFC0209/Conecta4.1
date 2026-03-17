7c1d5d5b00cfc729e924e650ee754c1c0a4ba534
Pau Febrer Castro <pfebrerc@gmail.com>
2026-03-17 12:28:15 +0100

    implementació del mètodo imprimirTablero i finalització a la clase Tablero

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,2 +1,13 @@
 public class Tablero {
+    public static void imprimirTablero(){
+        System.out.println("\n 0 1 2 3 4 5 6 ");
+        for (int i = 0; i < Juego.files; i++) {
+            System.out.print("|");
+            for (int j = 0; j < Juego.columnes; j++) {
+                System.out.print(Juego.tablero[i][j] + "|");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------");
+    }
 }
