fd84553c0c0f47a93bbc8d49322fff3b7803d1af
Pau Febrer Castro <pfebrerc@gmail.com>
2026-03-17 13:18:50 +0100

    prova del Tablero i creació de les variables dels jugadors tablero, files i columnes

--- src/Juego.java
+++ src/Juego.java
@@ -1,2 +1,22 @@
+import static
+
 public class Juego {
+    public static final int files = 6;
+    public static final int columnes = 7;
+    public static final char buit = ' ';
+    public static final char jugador1 = 'X';
+    public static final char jugador2 = 'O';
+
+    public static char tablero[][] = new char[files][columnes];
+
+    public static void main(String[] args){
+        for (int i = 0; i < files; i++) {
+            for (int j = 0; j < columnes; j++) {
+                tablero[i][j] = buit;
+            }
+        }
+        
+        Tablero.imprimirTablero();
+    }
+
 }
