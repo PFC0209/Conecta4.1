0d4fdf1f8c9abc7a67c840a11fc5c2218dea88b5
Pau Febrer Castro <pfebrerc@gmail.com>
2026-03-17 13:43:56 +0100

    Implementació de la comprovació de les diagonals

--- src/Guanyador.java
+++ src/Guanyador.java
@@ -9,12 +9,32 @@ public class Guanyador {
             }
         }
         //Comprovació vertical
-        for(int i = 0; i <= Juego.files -4; i++){
+        for(int i = 0; i <= Juego.files-4; i++){
             for(int j = 0; j < Juego.columnes; j++){
                 if(Juego.tablero[i][j] == jugador && Juego.tablero[i+1][j] == jugador && Juego.tablero[i+2][j] == jugador && Juego.tablero[i+3][j] == jugador){
                     return true;
                 }
             }
         }
+
+        //Comprovació diagonal (/)
+        for (int i = 3; i < Juego.files; i++) {
+            for (int j = 0; j < Juego.columnes-4; j++) {
+                if (Juego.tablero[i][j] == jugador && Juego.tablero[i-1][j+1] == jugador && Juego.tablero[i-2][j+2] == jugador && Juego.tablero[i-3][j+3] == jugador){
+                    return true;
+                }
+            }
+        }
+        
+        //Comprovació diagonal (\)
+        for (int i = 0; i < Juego.files-4; i++) {
+            for (int j = 0; j < Juego.columnes; j++) {
+                if (Juego.tablero[i][j] == jugador && Juego.tablero[i+1][j+1] == jugador && Juego.tablero[i+2][j+2] == jugador && Juego.tablero[i+3][j+3] == jugador) {
+                    return true;
+                }
+            }
+            
+        }
+        return false;
     }
 }
