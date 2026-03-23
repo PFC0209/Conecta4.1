954618a389ccc4d4cd820bbe0829dd23ee1aba24
Pau Febrer Castro <pfebrerc@gmail.com>
2026-03-23 08:43:28 +0100

    Implementació dle mètode jugar on surten les condicions segons els resultats

--- src/Juego.java
+++ src/Juego.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Juego {
     public static final int files = 6;
     public static final int columnes = 7;
@@ -5,16 +7,38 @@ public class Juego {
     public static final char jugador1 = 'X';
     public static final char jugador2 = 'O';
 
-    public static char tablero[][] = new char[files][columnes];
+    public static char tablero[][];
 
-    public static void main(String[] args){
+    public Juego(){
+        tablero= new char[files][columnes];
         for (int i = 0; i < files; i++) {
             for (int j = 0; j < columnes; j++) {
                 tablero[i][j] = buit;
             }
         }
-        
-        Tablero.imprimirTablero();
     }
 
+    public void jugar(){
+        Scanner sc = new Scanner(System.in);
+        char jugadorActual = jugador1;
+        boolean jocFinalitzat = false;
+
+        while(!jocFinalitzat){
+            Tablero.imprimirTablero();
+            int columna = Columna.demanaColumna(sc, jugadorActual);
+            Ficha.colocarFitxa(columna, jugadorActual);
+
+            if(Guanyador.hiHaGuanyador(jugadorActual)){
+                Tablero.imprimirTablero();
+                System.out.println("Jugador " + jugadorActual + " ha guanyat!");
+                jocFinalitzat = true;
+            }else if(Empate.esEmpate()){
+                System.out.println("És un empate");
+                jocFinalitzat = true;
+            }else{
+                jugadorActual = (jugadorActual == jugador1) ? jugador2 : jugador1;
+            }
+        }
+        sc.close();
+    }
 }
