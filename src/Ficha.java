51fa815770ab011442f0d55778db3c26c171f1d3
Pau Febrer Castro <pfebrerc@gmail.com>
2026-03-23 08:33:41 +0100

    Creació de la classe Ficha e implementació del mètode colocarFitxa

--- src/Ficha.java
+++ src/Ficha.java
@@ -1,3 +1,11 @@
 public class Ficha {
-
+    public static boolean colocarFitxa(int columna, char jugador){
+        for (int i = Juego.files - 1; i >=0 ; i--){
+            if (Juego.tablero[i][columna] == Juego.buit){
+                Juego.tablero[i][columna] = jugador;
+                return true;
+            }
+        }
+        return false;
+    }
 }
